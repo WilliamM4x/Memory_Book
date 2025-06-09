839603d9d0e6b79b8caf2f91f48005fb2fc0b598
Max <chwilha2@gmail.com>
2025-06-09 16:34:21 -0300

    Deployment functions

--- lib/plan_market_auth.dart
+++ lib/plan_market_auth.dart
@@ -1,4 +1,7 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
 class PlanMarketAuth with ChangeNotifier {
 
@@ -9,9 +12,53 @@ class PlanMarketAuth with ChangeNotifier {
   String _resource = "/v1/accounts";
   String _apiKey = "AIzaSyDMqk1S_fpBkMNZNvs2aeRI_bEQ4itYvS8";
 
-  void singUp(){
+  Future<bool> singUp(String email, String senha) async {
+      String string_uri = '$_url$_resource:singUp?key=$_apiKey';
+      Uri uri= Uri.parse(string_uri);
 
+      var response = await http.post(
+        uri,
+        body: json.encode({
+          'email': email,
+          'password': senha,
+          'returnSecureToken': 'true'
+        }),
+      );
+
+      if (response.statusCode == 200){
+        _message = "Cadastro realizdo com sucesso.";
+        var body = jsonDecode(response.body);
+        _token = body['idToken'];
+        return true;
+      } else {
+        _message = "Erro ao cadastrar.";
+        return false;
+      }
   }
-  void singIn(){
+
+
+  Future<bool> singIn(String email, String senha) async {
+    String string_uri = '$_url$_resource:singInWhithPassword?key=$_apiKey';
+    Uri uri= Uri.parse(string_uri);
+
+    var response = await http.post(
+      uri,
+      body: json.encode({
+        'email': email,
+        'password': senha,
+        'returnSecureToken': 'true'
+      }),
+    );
+
+    if (response.statusCode == 200){
+      _message = "Login realizado com sucesso.";
+      var body = jsonDecode(response.body);
+      _token = body['idToken'];
+      return true;
+    } else {
+      _message = "Erro ao realizar login.";
+      return false;
+    }
+
   }
 }
