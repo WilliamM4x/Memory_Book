2fcf7e41b0f8c196859b032ca0372709c44c7fd3
Max <chwilha2@gmail.com>
2025-06-11 15:02:51 -0300

    final ajuste3

--- lib/plan_market_auth.dart
+++ lib/plan_market_auth.dart
@@ -2,7 +2,7 @@ import 'dart:convert';
 
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
-class AuthProvider with ChangeNotifier{
+class PlanMarketAuth with ChangeNotifier{
   String? message;
   String? token;
 
