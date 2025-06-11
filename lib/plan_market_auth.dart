aa5d055f2189049c6f1a1308ccf69af78fcb9f7d
Max <chwilha2@gmail.com>
2025-06-11 15:01:07 -0300

    final ajuste2

--- lib/plan_market_auth.dart
+++ lib/plan_market_auth.dart
@@ -2,9 +2,7 @@ import 'dart:convert';
 
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
-
-class PlanMarketAuth with ChangeNotifier {
-
+class AuthProvider with ChangeNotifier{
   String? message;
   String? token;
 
@@ -12,54 +10,47 @@ class PlanMarketAuth with ChangeNotifier {
   final _resource = "/v1/accounts";
   final _apiKey = "AIzaSyDMqk1S_fpBkMNZNvs2aeRI_bEQ4itYvS8";
 
-  Future<bool> singUp(String email, String senha) async {
-      String string_uri = '$_url$_resource:singUp?key=$_apiKey';
-      Uri uri= Uri.parse(string_uri);
+  Future<bool> singUp (String email, String password) async {
+    String sUri = '$_url$_resource:signUp?key=$_apiKey';
+    Uri uri = Uri.parse(sUri);
 
-      var response = await http.post(uri, body: {
-          'email': email,
-          'password': senha,
-          'returnSecureToken': 'true'
-        });
+    var response = await http.post(uri, body: {
+      'email': email,
+      'password': password,
+      'returnSecureToken': 'true'
+    });
 
-      if (response.statusCode == 200){
-        message = "Cadastro realizdo com sucesso.";
-        var body = jsonDecode(response.body);
-        token = body['idToken'];
-        print("$message $token");
-        return true;
-      } else {
-        message = "Erro ao cadastrar.";
-        print(message);
-        return false;
-      }
-  }
-
-
-  Future<bool> singIn(String email, String senha) async {
-    String string_uri = '$_url$_resource:singInWithPassword?key=$_apiKey';
-    Uri uri= Uri.parse(string_uri);
-
-    var response = await http.post(
-      uri,
-      body: {
-        'email': email,
-        'password': senha,
-        'returnSecureToken': 'true'
-      }
-    );
-
-    if (response.statusCode == 200){
-      message = "Login realizado com sucesso.";
-      print(message);
+    if (response.statusCode==200){
+      message = "Cadastro realizdo com sucesso.";
       var body = jsonDecode(response.body);
       token = body['idToken'];
       return true;
-    } else {
-      message = "Erro ao realizar login.";
-      print("$message $token");
+    }else{
+      message = "Erro ao cadastrar.";
       return false;
     }
 
   }
+  Future<bool> singIn(String email, String password) async {
+    String sUri = '$_url$_resource:signInWithPassword?key=$_apiKey';
+    Uri uri = Uri.parse(sUri);
+
+    var response = await http.post(uri, body:{
+      'email': email,
+      'password': password,
+      'returnSecureToken': 'true'
+    });
+
+    if (response.statusCode==200){
+      message = "User autenticado com sucesso.";
+      var body = jsonDecode(response.body);
+      token = body['idToken'];
+      return true;
+    }else{
+      message = "Erro ao autenticar.";
+      return false;
+    }
+
+  }
+
 }
